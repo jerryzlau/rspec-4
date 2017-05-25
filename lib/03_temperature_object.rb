78fcc59b09cb2094678d8c79a34f78f5682cdcc6
Jerry Lau <jerryzlau@gmail.com>
2017-05-25 00:46:12 -0700

    implemented temperature_object

--- lib/03_temperature_object.rb
+++ lib/03_temperature_object.rb
@@ -1,3 +1,38 @@
 class Temperature
-  # TODO: your code goes here!
+  attr_accessor :options
+
+  def initialize(options)
+    self.options = options
+  end
+
+  def in_fahrenheit
+    return options[:f] if options[:f]
+    (options[:c] * 9.0/5) + 32
+  end
+
+  def in_celsius
+    return options[:c] if options[:c]
+    (options[:f] - 32.0) * 5/9
+  end
+
+  def self.from_celsius(temp_in_c)
+    self.new(:c => temp_in_c)
+  end
+
+  def self.from_fahrenheit(temp_in_f)
+    self.new(:f => temp_in_f)
+  end
+
+end
+
+class Celsius < Temperature
+  def initialize(c)
+    super(:c => c)
+  end
+end
+
+class Fahrenheit < Temperature
+  def initialize(f)
+    super(:f => f)
+  end
 end
