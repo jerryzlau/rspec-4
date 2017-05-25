70dba9a41c0ecd4382bafaab1aabd326218b2417
Jerry Lau <jerryzlau@gmail.com>
2017-05-25 14:50:43 -0700

    implemented dictionary

--- lib/04_dictionary.rb
+++ lib/04_dictionary.rb
@@ -1,3 +1,42 @@
+require "byebug"
+
 class Dictionary
   # TODO: your code goes here!
+  attr_reader :entries
+
+  def initialize
+    @entries = {}
+  end
+
+  def add(input)
+    if input.instance_of?(String)
+      @entries[input] = nil
+    else
+      @entries.merge!(input)
+    end
+  end
+
+  def keywords
+    # debugger
+    @entries.keys if @entries.keys.length == 1
+    @entries.keys.sort
+  end
+
+  def include?(key)
+    @entries.has_key?(key)
+  end
+
+  def find(input)
+    @entries.select {|k,v| k.include?(input)}
+  end
+
+  def printable
+    result = ""
+    sorted = @entries.sort_by {|k,v| k}
+    sorted.each do |k,v|
+      result << "[#{k}] \"#{v}\"" + "\n"
+    end
+    result.strip
+  end
+
 end
